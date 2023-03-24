b0112984fe8fa6e2b4a717596285a849a2fc549c
berglertecnologia <symondiksj@gmail.com>
2023-03-24 08:15:22 -0300

    alterando atributos2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-
 function clicou() {
-const input = document.querySelector('input');
-console.log(input)
-
+const input = document.querySelector('input'); const botao = document.querySelector('.botao');
+if (input.getAttribute('type') === 'text') { input.setAttribute('type', 'password'); botao.innerText = "Mostrar Senha";
+} else {
+input.setAttribute('type', 'text'); botao.innerText = "Ocultar Senha";
 }
-
+}
 
 
 
