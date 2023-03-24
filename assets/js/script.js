3dfd5715e627a0c19b0c27b413045ddac42d9b5a
berglertecnologia <symondiksj@gmail.com>
2023-03-24 08:21:58 -0300

    Evento de clique mostrar e ocultar senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function clicou() {
+// function clicou() {
 const input = document.querySelector('input'); const botao = document.querySelector('.botao');
 if (input.getAttribute('type') === 'text') { input.setAttribute('type', 'password'); botao.innerText = "Mostrar Senha";
 } else {
