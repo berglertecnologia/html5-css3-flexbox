e5b384fa817d04cc484c0b292bc09a3ce7ffe08e
berglertecnologia <symondiksj@gmail.com>
2023-04-03 07:56:24 -0300

    AddDigitadoInputNaTela3

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,11 @@ const lista = document.querySelector('ul');
 function handleKeyUp(e) {
 if (e.key === 'Enter') {
 const newli = document.createElement('li'); 
-newli.innerHTML = input.value;
 
+newli.innerHTML = input.value;
 lista.appendChild(newli);
+input.value = '';
+
 }
 
 }
